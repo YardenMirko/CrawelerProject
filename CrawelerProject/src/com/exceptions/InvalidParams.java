0a15b88f63ebb44ce97e22ecf49a2e8b3c800701
YardenMirko <83639847+YardenMirko@users.noreply.github.com>
2022-06-24 18:54:56 +0300

    Delete CrawelerProject directory

--- CrawelerProject/src/com/exceptions/InvalidParams.java
+++
@@ -1,9 +0,0 @@
-package com.exceptions;
-
-public class InvalidParams extends Exception {
-
-    public InvalidParams(String url, String number) {
-        super(String.format("Url - %s , number - %s are not valid", url, number));
-    }
-
-}
